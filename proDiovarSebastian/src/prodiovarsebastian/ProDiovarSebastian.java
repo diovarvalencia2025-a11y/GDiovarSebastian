6da92682aea3ee0378a29a9a56fa7288a5c14f4c
USUARIO <USUARIO@DESKTOP-7CUCEV9>
2026-04-14 23:17:52 +0200

    Primer cambio

---
+++ proDiovarSebastian/src/prodiovarsebastian/ProDiovarSebastian.java
@@ -0,0 +1,20 @@
+
+package prodiovarsebastian;
+
+
+public class ProDiovarSebastian {
+
+   
+    public static void main(String[] args) {
+        
+    }  
+    
+    
+        public static void saludar() {
+    System.out.println("Hola mundo!");
+}
+        
+       
+    }
+    
+
