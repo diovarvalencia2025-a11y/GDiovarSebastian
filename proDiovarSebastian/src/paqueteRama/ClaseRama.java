1c1aa5373a52cc13faa913ae4d86eeda4d97b51d
USUARIO <USUARIO@DESKTOP-7CUCEV9>
2026-04-16 23:27:13 +0200

    Primer cambio Segundo Cambio Tercer cambio para el remoto Cuarto Cambio Añadido paquete y clase nueva en rama

---
+++ proDiovarSebastian/src/paqueteRama/ClaseRama.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqueteRama;
+
+
+
+public class ClaseRama {
+    
+    
+    public static void mensajeRama() {
+        System.out.println("Este mensaje está en la rama ramaDiovarSebastian");
+    }
+}
+    
+
